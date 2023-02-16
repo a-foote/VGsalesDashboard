67ae00a6c9ce8a7239d6f947337f5d95ec1a1d6a
jonahedmundson <jonahedmundson>
2023-02-15 17:04:42 -0800

    Added files.

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -3,9 +3,10 @@ import pandas as pd
 df = pd.read_csv('./vgsales.csv')
 
 #need to:
+	#remove rows with year 2017 and 2020
 	#group platform by company, with 'other' category (PC, playstation, xbox, nintendo, sega, (atari), other)
 	#group publisher to have 'other' category (8 categories ish)
-	
+
 
 
 
