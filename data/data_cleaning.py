285bb5f00f4371d85d1c3f1aaf3d71e9053c6393
jonahedmundson <jonahedmundson>
2023-02-15 16:25:54 -0800

    Added files.

---
+++ data/data_cleaning.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('./vgsales.csv')
+
+#need to:
+	#group platform by company, with 'other' category (PC, playstation, xbox, nintendo, sega, (atari), other)
+	#group publisher to have 'other' category (8 categories ish)
+	
+
+
+
+
+
+#output to csv
