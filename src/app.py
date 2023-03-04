dbd4323ca8fd0110f8c9f1781c9784dca1def69c
jonahedmundson <jonahedmundson>
2023-03-04 14:14:24 -0800

    Initial app boilerplate

--- src/app.py
+++ src/app.py
@@ -3,7 +3,6 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc
 
-#web: gunicorn src.app:server
 
 app = dash.Dash(__name__,  external_stylesheets= [dbc.themes.UNITED], suppress_callback_exceptions=True,use_pages = True)
 server = app.server
