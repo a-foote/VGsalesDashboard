09fe7eb269094e6a1e1e4fe7b30d2828573ddc47
a-foote <alyssa.k.foote@gmail.com>
2023-03-15 15:22:16 -0700

    add comments to app and page1

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import html, dcc
 
-
+#initialize a dash app
 app = dash.Dash(__name__,  external_stylesheets= [dbc.themes.UNITED], suppress_callback_exceptions=True,use_pages = True, title='Video Game Analysis')
 server = app.server
 
+#setup general layout
 app.layout = dbc.Container(children=[
                 dbc.Row(children=[
                     dbc.Col(children=[
+                        #app title
                         html.H1("Video Game Sales: A Historical Analysis", style={"background-color":"#95a5a6","color":"#c9020f",'border': '5px solid #828282','padding':0})
                     ])
                 ]),
@@ -17,6 +19,7 @@ app.layout = dbc.Container(children=[
                     dbc.Col(children=[
                         dbc.Nav(
                             [
+                                #create tabs
                                 dbc.NavItem(dbc.NavLink(page['name'], href=page['path'],active="exact"))
                                 for page in dash.page_registry.values()
                             ],
@@ -24,10 +27,11 @@ app.layout = dbc.Container(children=[
                         )
                     ])
                 ]),
+                #call current page
                 dbc.Row(children=[dash.page_container])
             ],fluid=True,
             style={'height': '100vh', 'background-image': 'url(/assets/background.jpg)','background-size': '100%', 'position': 'fixed',}
 )
-
+#run app
 if __name__ == '__main__':
     app.run_server()
--- pages/pg1_sales.py
+++ pages/pg1_sales.py
@@ -8,23 +8,25 @@ import altair as alt
 import pandas as pd
 
 from plot_functions import map
-
+#connect page to app.py
 dash.register_page(__name__,path='/',name="Global Sales")
-
+#read data
 pg1_data = pd.read_csv("data/vgsales-cleaned.csv", parse_dates=['Year'])
 pg1_data['Year'] = pg1_data['Year'].dt.year.astype('Int64')
 
 alt.data_transformers.disable_max_rows()
-
+#design layout
 layout = dbc.Container(
         children=[
+            #row 1
             dbc.Row([
                 dbc.Col(children=[
                 dcc.Graph(
                         id='map',
                         style={'float': 'left', 'border-width': '0'},
                     )], style={'float':'left'})
-            ]),
+            ]), #end of row1
+            #row2
             dbc.Row([
                 dbc.Col(children=[
                     html.Iframe(
@@ -35,17 +37,26 @@ layout = dbc.Container(
                         dcc.RangeSlider(id='gs_slider', min=1980, max=2017, value=[1980,2016], step=1, marks=None, tooltip={"placement": "bottom", "always_visible": True})
                     ], style={'width':'50vw'})
                 ])
-            ])
+            ]) #end of row2
         ]
     )
 
-    # Set up callbacks/backend
+# Set up callbacks/backend
 @dash.callback(
     [Output('scatter', 'srcDoc'),
     Output('map','figure')],
     [Input('gs_slider', 'value')]
 )
 def sales_time(years):
+"""
+A function that creates a line plot in Altair.
+
+    Parameters:
+    years (array of int): an array of two integers, used to filter data
+
+    Returns:
+    plot (altair plot): An HTML formatted Altair line plot
+"""
     plot = (alt.Chart(pg1_data[(pg1_data['Year'] > years[0]) & (pg1_data['Year'] < years[1])], title='Total Regional Sales').mark_line().encode(
                     alt.X('Year',scale=alt.Scale(zero=False), title=None, axis=alt.Axis(format='d')),
                     alt.Y('sum(Global_Sales)', title='Sum of Sales (millions)', axis=alt.Axis(format='$s'))
@@ -55,6 +66,7 @@ def sales_time(years):
                 )#.mark_errorband(extent='ci')
            )
 
+# To Do - Add confidence band to line plot
 #         band = (alt.Chart(df).mark_errorband(extent='ci').encode(
 #                         x='Year',
 #                         y='Global_Sales'
